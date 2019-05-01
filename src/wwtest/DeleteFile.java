37a72024ac74ba75169b6ca7e00b1bc4867815c3
王威 <269099591@qq.com>
2019-05-01 09:48:31 +0800

    各种算法实现

---
+++ src/wwtest/DeleteFile.java
@@ -0,0 +1,5 @@
+package wwtest;
+
+public class DeleteFile {
+
+}
---
+++ src/wwtest/jdbc.java
@@ -0,0 +1,45 @@
+package wwtest;
+import com.mysql.jdbc.*;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class jdbc {
+	public static void main(String args[]) {
+	String driver = "com.mysql.jdbc.Driver";
+    String url = "jdbc:mysql://localhost:3306/library";
+    String username = "root";
+    String password = "123456";
+    Connection conn = null;
+    try {
+        Class.forName(driver); //classLoader,加载对应驱动
+        conn = (Connection) DriverManager.getConnection(url, username, password);
+        
+        String sql = "select * from book";
+        PreparedStatement pstmt;
+        try {
+            pstmt = (PreparedStatement)conn.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+            int col = rs.getMetaData().getColumnCount();
+            System.out.println("============================");
+            while (rs.next()) {
+                for (int i = 1; i <= col; i++) {
+                    System.out.print(rs.getString(i) + "\t");
+                    if ((i == 2) && (rs.getString(i).length() < 8)) {
+                        System.out.print("\t");
+                    }
+                 }
+                System.out.println("");
+            }
+                System.out.println("============================");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+    
+	}
+
+}
